7c3b0c237d34ab8c1d3e10d2a8f02acf7d4f11be
Flynn <flynn@flynnconnolly.com>
2020-05-20 19:51:42 -0700

    gun flip
    gun now flips to face the direction of the mouse. Doesn't shoot just yet.

---
+++ Assets/Scripts/FaceMouse.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.GameCenter;
+
+public class FaceMouse : MonoBehaviour
+{
+    Vector2 movement;
+    public Rigidbody2D rb;
+    public Vector2 flip;
+    public SpriteRenderer renderer;
+
+   void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        faceMouse();
+    }
+
+    void faceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        if (mousePosition.x > gameObject.transform.parent.position.x)
+        {
+            gameObject.transform.localPosition = new Vector2(0.83f, -0.02f);
+            renderer.flipY = false;
+        }
+        else
+        {
+            gameObject.transform.localPosition = new Vector2(-0.83f, -0.02f);
+            renderer.flipY = true;
+        }
+
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y);
+        transform.right = direction;
+    }    
+}
---
+++ Assets/Scripts/move2D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class move2D : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal"); 
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
